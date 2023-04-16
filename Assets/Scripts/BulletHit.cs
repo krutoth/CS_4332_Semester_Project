3308a7132fb88cb63c4dbd899f115d9dcba1559d
krutoth <krutoth01@gmail.com>
2023-04-16 07:10:53 -0500

    Progress
    Particle Effects, Scene edits

--- Assets/Scripts/BulletHit.cs
+++ Assets/Scripts/BulletHit.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BulletHit : MonoBehaviour
 {
+    public GameObject particle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +19,18 @@ public class BulletHit : MonoBehaviour
     }
 
     //When we touch the collider we disable this object.
-    void OnCollisionEnter()
+    void OnCollisionEnter(Collision other)
     {
+        //Find the contact point on the object we collided with.
+        ContactPoint contact = other.contacts[0];
+
+        //Set the exact position and rotation we hit the collider at.
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+
+        //Spawn our particle using the above parameters.
+        Instantiate(particle, pos, rot);
+
         gameObject.SetActive(false);
     }
 }
